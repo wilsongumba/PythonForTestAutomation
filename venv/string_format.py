7a1b4f7eabc50c3a2c7efe7ee31be58b73964ec4
wilson <wilsongumba@gmail.com>
2020-06-07 02:39:00 +0300

    Initial commit

---
+++ venv/string_format.py
@@ -0,0 +1,30 @@
+# single-line comment
+
+"""
+multi-line
+comment
+"""
+
+'''
+multi-line
+comment 2
+'''
+
+#variables
+
+name = "wilson gumba"
+First_name = "wilson"
+lastName = "gumba"
+_age = 500
+weight = 800.08
+
+print(name)
+
+print(First_name + lastName)
+
+print(First_name +" "+ lastName)
+
+print(First_name +" "+ lastName + ".")
+
+#formating with string.format()
+print("My name is {} {},I am {}years old and I weigh {}kgs.".format(First_name, lastName, _age, weight))
